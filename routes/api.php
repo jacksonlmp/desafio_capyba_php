391c74b7b4d7f32b08ac1a49aa1c4417b656dd27
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-09-19 15:17:14 -0300

    Routes

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,15 @@ use App\Http\Controllers\UserController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware(['auth:sanctum', 'verified'])->get('/rota-protegida', function () {
+    return response()->json(['message' => 'Acesso permitido!']);
+});
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->post('/user/change-password', [UserController::class, 'updatePassword']);
 Route::middleware('auth:sanctum')->put('/user/update', [UserController::class, 'updateProfile']);
+Route::middleware('auth:sanctum')->post('/email/send-token', [AuthController::class, 'sendVerificationToken']);
+Route::middleware('auth:sanctum')->post('/email/verify', [AuthController::class, 'verifyEmail']);
