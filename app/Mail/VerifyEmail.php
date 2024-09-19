5993bf7adb0df604b9a2c4e47bec6fed26af7d09
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-09-19 15:16:41 -0300

    Method to send e-mail with infos

---
+++ app/Mail/VerifyEmail.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class VerifyEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $user;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    public function build()
+    {   
+        return $this->subject('Confirme seu e-mail')
+                    ->view('emails.verify-email') // Defina uma view para o conteúdo do e-mail
+                    ->with(['token' => $this->user->verification_token]);
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Verificar e-mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    // public function content(): Content
+    // {
+    //     return new Content(
+    //         view: 'view.emails.verify-email',
+    //     );
+    // }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
